2debcdd3b6c7bf8ca0621decf53a14bf89a1e922
Alex Kruchkoff <alexk@noclue.local>
2015-12-15 19:15:24 +1100

    +files

--- puppet/etc/puppet/modules/bind/init.pp
+++ puppet/etc/puppet/modules/bind/init.pp
@@ -2,5 +2,27 @@ class bind {
   package { 'bind':
     ensure => present,
   }
+
+  file { '/etc/named.conf':
+    owner   => 'root',
+    group   => 'named',
+    mode    => '0640',
+    source  => 'puppet:///modules/bind/etc/named.conf',
+  }
+
+  file { '/var/named/ak.local':
+    owner   => 'root',
+    group   => 'named',
+    mode    => '0640',
+    source  => 'puppet:///modules/bind/var/named/ak.local',
+  }
+  }
+
+  file { '/var/named/2.168.192-in-addr.arpa':
+    owner   => 'root',
+    group   => 'named',
+    mode    => '0640',
+    source  => 'puppet:///modules/bind/var/named/2.168.192-in-addr.arpa',
+  }
 }
 
