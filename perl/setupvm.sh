f0d7ade8587b63c5b5df3955145b943e17f9ffb8
Alex Kruchkoff <alexk@noclue.local>
2015-12-27 14:45:20 +1100

    a few VBoxManage commands commented out

--- perl/setupvm.sh
+++ perl/setupvm.sh
@@ -73,12 +73,38 @@ if ($debug) {
 }
 
 if (exists $uuid{$name}) {
-	print STDERR "vm \"$name\" already exists\n" if $debug;
-	$command = "$VBoxManage showvminfo $name 2>&1";
+	print STDERR "vm \"$name\" already exists with uuid: \"$uuid{$name}\"\n"
+		if $debug;
+	$command = "$VBoxManage showvminfo \"$uuid{$name}\" 2>&1";
 	($error, @output) = from $command;
 	die "got an error: $error " . join ("\n", @output) . ".\n" if $error;
 	for my $line (@output) {
 		print STDERR "\"$line\"\n" if $debug;
 	}
+} # check if exists uuid
+else { # new VM
+	$command = "$VBoxManage createvm --name \"$name\" --register 2>&1";
+	($error, @output) = from $command;
+	print STDERR "command: \"$command\"\n" if $debug;
+	die "got an error: $error " . join ("\n", @output) . ".\n" if $error;
+	for my $line (@output) {
+		print STDERR "\"$line\"\n" if $debug;
+		# get uuid
+	}
+	# --boot1 disk
+	# --boot2 net
+	# --boot<1-4> none|floppy|dvd|disk|net
+	# VBoxManage modifyvm \"$name\" --boot1 disk --boot2 net --boot3 none --boot4 none --ioapic on --nic1 nat --nictype1 82540EM --nattftpfile1 /pxelinux.0 --natsettings1 16000,128,128,0,0 --natdnshostresolver1 on --uart1 0x3F8 4 --uartmode1 file /tmp/\"$name\".serial --rtcuseutc on --ostype RedHat_64  --usb on --usbehci on
+	# VBoxManage modifyvm ento --natpf1 "guestssh,tcp,,6666,,22" # which port 6666 ?
+	# VBoxManage storagectl \"$name\" --name IDE --add ide --controller PIIX4 --portcount 2 --bootable on
+	# VBoxManage storagectl \"$name\" --name SATA --add sata --controller IntelAhci  --portcount 1 --bootable on
+	# VBoxManage storageattach \"$name\" --storagectl IDE --port 1 --device 0 --medium emptydrive
+	#
+	# VBoxManage createmedium disk --filename "/Users/alexk/VirtualBox VMs/ento/ento.vdi" --size 204800 --format VDI --variant Standard; echo $?
+	#0%...10%...20%...30%...40%...50%...60%...70%...80%...90%...100%
+	Medium created. UUID: b10000a6-4500-4160-8672-56082cd9ecfc
+	#0
+	#
+  # VBoxManage storageattach ento --storagectl SATA --port 0 --device 0 --type hdd --medium b10000a6-4500-4160-8672-56082cd9ecfc
 }
 
