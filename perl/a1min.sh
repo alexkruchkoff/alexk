01a966f3d60bb9774d62d98dc305eb62025572a1
Alex Kruchkoff <Alex Kruchkoff alex@kruchkoff.com>
2016-08-12 14:54:32 +1000

    calculate min/max/avg

--- perl/a1min.sh
+++ perl/a1min.sh
@@ -1,18 +1,40 @@
+use POSIX;
+
 #0              1 2 3                     4      5     6     7      8         9   10   11    12                   13  14 
 #10.110.157.120 - - [08/Jul/2016:11:14:56 +1000] 48543 "POST /b/c/d HTTP/1.1" 200 1279 TLSv1 ECDHE-RSA-AES256-SHA "-" "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)" "0" "-"
 
 my $datePos = 3; 
+my $durationPos = 5;
+my $duration;
 my $codePos = 9; 
 my $total = 0;
-my (%lpm, %codes, %lpmcode, %total) = ();
+my (%lpm, %codes, %lpmcode, %total, %min, %max, %avg) = ();
 chomp(my @lines = <>);
 for my $line (@lines) {
 	my @fields = split / /, $line;
-	my $m10 = substr($fields[$datePos], 13, 5);
+	my $m1 = substr($fields[$datePos], 13, 5);
+	$duration = 0 + $fields[$durationPos];
+
+	if(exists($min{"$m1"})) {
+		if($duration < $min{"$m1"}) {
+			$min{"$m1"} = $duration;
+		}
+
+		if($duration > $max{"$m1"}) {
+			$max{"$m1"} = $duration;
+		}
+
+		$avg{"$m1"} += $duration;
+	}
+	else {
+		$min{"$m1"} = $duration;
+		$max{"$m1"} = $duration;
+		$avg{"$m1"} = $duration;
+	}
 	$codes{$fields[$codePos]} = 1;
-  $lpm{"$m10"}++;
+  $lpm{"$m1"}++;
 	$total++;
-  $lpmcode{"$m10$fields[$codePos]"}++;
+  $lpmcode{"$m1$fields[$codePos]"}++;
   $total{"$fields[$codePos]"}++;
 }
 
@@ -20,7 +42,7 @@ print "time\ttotal";
 for my $code (sort keys %codes) {
 	print "\t$code";
 }
-print "\n";
+print "\tmin\tmax\tavg\n";
 
 for my $ten (sort keys %lpm) {
 	print "$ten\t$lpm{$ten}";
@@ -34,7 +56,8 @@ for my $ten (sort keys %lpm) {
 			print "\t0";
 		}	
 	}
-	print "\n";
+	print "\t" . $min{"$ten"} . "\t" . $max{"$ten"} . "\t" .
+	floor($avg{"$ten"}/$lpm{"$ten"}) . "\n";
 }
 
 print "total\t$total";
